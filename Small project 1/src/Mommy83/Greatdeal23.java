8fae12064b7b349931ae7413312283f788d8dbc1
Lizshen83 <eshenkute@yahoo.com>
2022-05-07 19:39:38 -0400

    I love Naomi

---
+++ Small project 1/src/Mommy83/Greatdeal23.java
@@ -0,0 +1,10 @@
+package Mommy83;
+
+public class Greatdeal23 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello class of April");
+	}
+
+}
